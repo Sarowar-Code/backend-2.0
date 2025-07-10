78c374e17a2372b6a61b2d1a387e83b5287ef43e
Sarowar-h <sarw838dev@gmail.com>
2025-07-10 17:18:28 +0600

    Add refresh token functionality and improve JWT verification

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,30 +1,38 @@
-import {asyncHandler }from "../utils/asyncHandler"
-import jwt from "jsonwebtoken"
-import {User} from "../models/user.model"
-import {ApiError} from "../utils/ApiError.js"
- 
-export const verifyJWT =asyncHandler (req, _, next) => {
-try {
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+  // ** Middleware to verify JWT token **
+  try {
     // Check for token in cookies or Authorization header
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Barer ", "")
-    
-    if(!token) {
-        throw new ApiError(401, "Unothorized, No token provided")
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Barer ", "");
+    console.log("Token:", token);
+
+    if (!token) {
+      throw new ApiError(401, "Unothorized, No token provided");
     }
     // Verify the token
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-    
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
     // If the token is invalid or expired, jwt.verify will throw an error
 
-    if(!user) {
-        throw new ApiError(401, "Invalid token, user not found")
+    if (!user) {
+      throw new ApiError(401, "Invalid token, user not found");
     }
-    
-    req.user = user 
-    next()
-} catch (error) {
-    throw new ApiError(401, error?.message || "Unauthorized, Invalid access token") 
-}
 
-}
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(
+      401,
+      error?.message || "Unauthorized, Invalid access token"
+    );
+  }
+});
